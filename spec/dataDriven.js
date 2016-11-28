7020de62e26b3c5c2cdfc63ebe6f8a15e73c714b
Nelson Omuto <nelsonomuto@gmail.com>
2016-11-28 17:01:40 -0500

    NGDC-3404
    addless mock.js data driven refactored tests to derive from ux-ba-test

---
+++ spec/dataDriven.js
@@ -0,0 +1,114 @@
+describe('Data driven mock.js', function () {
+
+    before(function () {
+        driver = dataDriven('base/bridgevine/ux-ba-test/addless/mock.js/mock.json');
+    });
+
+    driver.it('data driven specs', function () {
+        // provide driver metadata
+        driver.setContextMetadata({
+                setupItemsId: 'xhrMockMethods',
+                expectationsId: 'expected',
+                otherData: ['locationHash']
+            })
+
+            // provide custom helper function to provide context for test execution
+            .setHelperFunc(function helperFunc (lastScope, execute) {
+                if(typeof lastScope.x === 'undefined') {
+                    location.hash = '';
+                    XMLHttpRequest.dropAllMocks();
+                    this.x = new XMLHttpRequest();
+                    this.setRequestHeaderArgs = [];
+                } else {
+                    this.x = lastScope.x;
+                    this.xhrChain = lastScope.xhrChain;
+                    this.setRequestHeaderArgs = lastScope.setRequestHeaderArgs;
+                }
+
+                var scope = setUpTestScope.apply(this);
+
+                if(typeof execute === 'function') {
+                    this.x.open('GET', 'base/addless/mock.js/spec/mock.json');
+                    if(this.locationHash) {
+                        location.hash = this.locationHash;
+                    }
+                    execute(scope);
+                }
+
+                return scope;
+
+                function setUpTestScope() {
+                    var mockFunction = this.fname;
+                    var mockFunctionArgs = this.fargs;
+
+                    if (mockFunction === 'setRequestHeader') {
+                        this.setRequestHeaderArgs.push(mockFunctionArgs);
+                        return this;
+                    }
+
+                    if (typeof XMLHttpRequest[mockFunction] === 'undefined' || mockFunction === 'setRequestHeader') {
+                        return this;
+                    }
+
+                    if (this.xhrChain) {
+                        this.xhrChain = this.xhrChain[mockFunction].apply(this.xhrChain, mockFunctionArgs);
+                    } else {
+                        this.xhrChain = XMLHttpRequest[mockFunction].apply(XMLHttpRequest, mockFunctionArgs);
+                    }
+
+                    return this;
+                }
+            })
+
+            // execute tests
+            .executeTests(function executeFn (done) {
+                var x = this.x;
+                var expectations = this.expectations;
+
+                setRequestHeaders.apply(this);
+                x.onload = onload;
+                x.send();
+
+                function onload() {
+                    var expectedValue;
+                    for(var key in expectations) {
+                        expectedValue = expectations[key];
+
+                        switch(key){
+                            case 'allResponseHeaders':
+                                expect(this.getAllResponseHeaders()).toBe(expectedValue);
+                                break;
+                            case 'responseBody':
+                                expect(this.responseText).toBe(expectedValue);
+                                break;
+                            case 'responseStatus':
+                                expect(this.status).toBe(expectedValue);
+                                break;
+                            case 'responseHeader':
+                                var actual, expected;
+                                for(var h in expectedValue){
+                                    actual = this.getResponseHeader(h);
+                                    expected = expectedValue[h];
+                                    expect(actual).toBe(expected);
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    done();
+                }
+
+                function setRequestHeaders() {
+                    var x = this.x;
+                    var setRequestHeaderArgs = this.setRequestHeaderArgs;
+                    var fargs;
+
+                    for(var i = 0; i < setRequestHeaderArgs.length; i++) {
+                        fargs = driver.parse(setRequestHeaderArgs[i]);
+                        x.setRequestHeader.apply(x, fargs);
+                    }
+                }
+            });
+    });
+});
